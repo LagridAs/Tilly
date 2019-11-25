0c0f74b4cac133fde7266e3fc8d5da67b7ba3234
SeyfGoumeida <goumeidaahmedseyfeddine@gmail.com>
2019-11-25 08:54:40 +0100

    [method] showLogout

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -13,8 +13,12 @@ public class UserController {
 	@RequestMapping("/login")
 	public String showLogin() {
 		return "login";
-
 	}	
+	
+	@RequestMapping("/loggedout")
+	public String showLogout() {
+		return "loggedout";
+	}
 	
 }
 
